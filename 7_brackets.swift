9cac1f682d38ed3d473f9aa4c66e4602236b4a17
Inushima <t.inushima@gmail.com>
2022-07-23 09:14:03 +0900

    Create 7_brackets.swift

---
+++ 7_brackets.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+var input =  "([)()]"
+
+
+public struct Stack<T> {
+  var array = [T]()
+  public var isEmpty: Bool {
+    return array.isEmpty
+  }
+
+  public mutating func push(_ element: T) {
+    array.append(element)
+  }
+
+  public mutating func pop() -> T? {
+    return array.popLast()
+  }
+
+  public var peek: T? {
+    return array.last
+  }
+}
+
+public func solution(_ S : inout String) -> Int {
+    let blackets = [
+        ")": "(",
+        "}": "{",
+        "]": "[",
+    ]
+    var stack = Stack<String>()
+
+    let close = blackets.keys
+    let open = blackets.values
+
+    for i in S {
+        let processing = i.description
+        if open.contains(processing) {
+            stack.push(processing)
+        }
+        if close.contains(processing) {
+            if let last = stack.peek, last == blackets[processing] {
+                stack.pop()
+            }
+        }
+    }
+
+    return stack.isEmpty ? 1 : 0
+}
+
+print(solution(&input))
